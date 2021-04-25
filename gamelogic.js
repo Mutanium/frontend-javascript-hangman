71979cd0c7e79ccbb4bfd245ddb86fa689a7a7fd
Mutanium <info@gaetan.nl>
2021-04-25 22:55:24 +0200

    2 tests passed, 4 to go

--- gamelogic.js
+++ gamelogic.js
@@ -1,5 +1,19 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+
+  //declareer string met streepjes
+  let wordPlaces = "";
+
+  //itereer wordlengte lang en log "_ "
+  for (let i = 0; i < word.length; i++) {
+    //als de i'de letter van het woord voorkomt in de guesses, toon dan de letter op de i'de positie
+    if (guesses.includes(word[i])) {
+      wordPlaces += word[i]+" ";
+      //plaats op de overige i's van het woord een underscore en een spatie
+    } else {
+      wordPlaces += "_ ";
+    }
+  }
+  return wordPlaces;
 }
 
 function isGameWon(word, guesses) {
